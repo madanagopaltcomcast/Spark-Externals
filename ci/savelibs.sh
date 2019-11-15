88e51d499d1acb43660107b937600222a3cbd9ef
madanagopalt <madanagopal123@gmail.com>
2019-11-15 07:28:30 +0530

    Update savelibs.sh

--- ci/savelibs.sh
+++ ci/savelibs.sh
@@ -25,7 +25,7 @@ NODE_MODULES_PATH=${EXT_INSTALL_PATH}/node_modules
 git checkout $TRAVIS_BRANCH
 REPO_USER_NAME=`echo $TRAVIS_REPO_SLUG | cut -d'/' -f 1`
 REPO_NAME=`echo $TRAVIS_REPO_SLUG | cut -d'/' -f 2`
-git pull https://$REPO_USER_NAME:$GH_TOKEN@github.com/$REPO_USER_NAME/$REPO_NAME.git master
+git pull https://$REPO_USER_NAME:$GH_TOKEN@github.com/$REPO_USER_NAME/$REPO_NAME.git tt
 git add ${EXT_INSTALL_PATH}
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]
 then
